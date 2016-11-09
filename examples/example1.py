5eee52fb90ae66c9fff5213bf18fb3978adb1ace
mbohlool <mehdy@google.com>
2016-11-09 14:21:34 -0800

    Update example

--- examples/example1.py
+++ examples/example1.py
@@ -21,12 +21,15 @@ import os
 # Configs can be set in Configuration class directly or using helper utility
 k8sutil.load_kube_config(os.environ["HOME"] + '/.kube/config')
 
+k8sclient.configuration.debug = True
+
 # Prior to python 3.4 hosts with ip-addresses cannot be verified for SSL. this
 # utility function fixes that.
 k8sutil.fix_ssl_hosts_with_ipaddress()
 
 v1=k8sclient.CoreV1Api()
 print "Listing pods with their IPs:"
-ret = v1.list_core_v1_pod_for_all_namespaces(watch=False)
+ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
-  print "%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name)
+    print "%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name)
+
