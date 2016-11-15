9b03295badb3e2ad58ff224525df6cf83def61ee
mbohlool <mehdy@google.com>
2016-11-15 03:30:18 -0800

    Fix SSL hosts check failure for ipaddresses as host

--- k8sutil/__init__.py
+++ k8sutil/__init__.py
@@ -13,3 +13,4 @@
 # limitations under the License.
 
 from .kube_config import load_kube_config
+from .ssl import fix_ssl_hosts_with_ipaddress
---
+++ k8sutil/ssl.py
@@ -0,0 +1,61 @@
+# Copyright 2016 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import urllib3
+import ipaddress
+import sys
+
+from six import PY3
+
+prev_match_hostname = None
+
+
+def _to_unicode(obj):
+    if isinstance(obj, str) and sys.version_info < (3,):
+        obj = unicode(obj, encoding='ascii', errors='strict')
+    return obj
+
+
+def _ip_address(str):
+    return ipaddress.ip_address(_to_unicode(str).rstrip())
+
+
+def _match_hostname(cert, hostname):
+    global prev_match_hostname
+    if not prev_match_hostname:
+        raise TypeError("prev_match_hostname should not be None")
+    try:
+        # Divergence from upstream: ipaddress can't handle byte str
+        host_ip = _ip_address(hostname)
+        san = cert.get('subjectAltName', ())
+        for key, value in san:
+            if key == 'IP Address':
+                if host_ip is not None and _ip_address(value) == host_ip:
+                    return
+    except ValueError:
+        pass
+    return prev_match_hostname(cert, hostname)
+
+def fix_ssl_hosts_with_ipaddress():
+    """urllib3 match_hostname does not support IP addresses. This function is adding the support."""
+
+    # Python 3 already supports this
+    if not PY3:
+        global prev_match_hostname
+        if getattr(getattr(urllib3, "connectionpool", None), "match_hostname", None):
+            prev_match_hostname=urllib3.connectionpool.match_hostname
+            urllib3.connectionpool.match_hostname = _match_hostname
+        if getattr(getattr(urllib3, "connection", None), "match_hostname", None):
+            prev_match_hostname=urllib3.connection.match_hostname
+            urllib3.connection.match_hostname = _match_hostname
